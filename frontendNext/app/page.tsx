8231fe6389ae565c4e139afc4d61dc31e39ae70f
theayusharma <protocolayush@gmail.com>
2025-08-19 01:40:12 +0530

    basic ui is up

--- frontendNext/app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-      <div className="bg-blue-200">
-      hindcnds
-      </div>
-  );
-}
---
+++ frontendNext/src/app/reduxstate/api.ts
@@ -0,0 +1,113 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { error } from "console";
+import { PagesUserlandModule } from "next/dist/server/route-modules/pages/module.compiled";
+
+export interface Project {
+    id? :string;
+    name?: string;
+		description?:string;
+		startdate?:string;
+		enddate?:string;
+
+}
+
+export interface Task {
+
+	id: number; 
+	createdat?: number; 
+	updatedat?: number; 
+	title: string; 
+	description?: string; 
+	status?: Status; 
+	priority?: Priority; 
+	tags?: string; 
+	startdate?: string; 
+	duedate?: string; 
+	points?: number; 
+	projectid: number; 
+	authorid?: number; 
+	assigneeid?: number; 
+  
+  author?:User;
+  assignee?:User;
+  comments?:Comment[];
+  attachments?:Attachment[]
+
+}
+export interface Attachment {
+  id : number;
+  fileUrl: string;
+  fileName: string;
+  taskId : number;
+  uploadedId : number;
+}
+
+export interface User {
+  userIdd?: number;
+  username: string;
+  email:string;
+  profilePictureUrl?:string;
+  providerId?:string;
+  teamId?:number;
+}
+
+export enum Status {
+  todo = "To Do",
+  workinprog = "Work In Progress",
+  completed = "Completed",
+  underreview = "Under Review"
+
+}
+export enum Priority {
+  urgnet = "Urgnet",
+  backlog = "Backlog",
+  low = "Low",
+  high = "High",
+  medium = "Medium"
+}
+
+
+
+export const api = createApi({
+    baseQuery : fetchBaseQuery({baseUrl : process.env.NEXT_PUBLIC_BACK_URL }),
+    reducerPath : 'api',
+    tagTypes : ["Projects", "Tasks"],
+    endpoints: (build) => ({
+    getProjects:build.query<Project[],void>({
+      query : () => "projects" ,
+      providesTags : ["Projects"],
+    }),
+    createProject : build.mutation<Project, Partial < Project>>({
+      query:(project) => ({
+        url : "projects",
+        method : "POST",
+        body : project
+      }),
+      invalidatesTags : ["Projects"], 
+    }),
+    getTasks:build.query<Task[],{projectId : number}>({
+      query : ( {projectId}) => `tasks?ProjectId = ${projectId}` ,
+      providesTags : (result) => result ? result.map(({id}) => ({type:"Tasks" as const, id })) : [{type : "Tasks" as const}]
+    }),
+    createTask  : build.mutation<Task, Partial < Task>>({
+      query:(task ) => ({
+        url : "tasks ",
+        method : "POST",
+        body : task 
+      }),
+      invalidatesTags : ["Tasks"], 
+    }),
+    updateTaskStatus : build.mutation<Task, {taskId : number; status : string}>({
+      query : ({taskId, status}) => ({
+        url : `tasks/${taskId}/status`,
+        method : "PATCH",
+      body : {status}, 
+      }),
+      invalidatesTags : (result, error, {taskId}) => [{
+        type : "Tasks", id: taskId
+      }],
+    })
+  }),
+})
+
+export const {useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation, useUpdateTaskStatusMutation} = api;
