388b43a0b68c108f65951c2ec0453c1858506f06
theayusharma <protocolayush@gmail.com>
2025-08-31 02:51:15 +0530

    added : list view

--- frontendNext/src/app/projects/page.tsx
+++ frontendNext/src/app/projects/page.tsx
@@ -1,28 +1,29 @@
 "use client"
-import React, {use, useState} from "react"
+import React, { useState } from "react"
 import ProjectHeader from "@/app/projects/ProjectHeader"
+import Boardview from "./Boardview";
+import List from "./ListView";
 type Props = {
-  params : {id :string};
+  params: { id: string };
 };
 
-const Project = (props: Props) => {
-  const {id} = params;
+const Project = ({ params }: Props) => {
+  const { id } = params;
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
- 
+
 
   return (
     // modal tasks
     <div>
-      <div>
-        Project   
-      </div>
-      <div>
-        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        {/* {activeTab == "Board" && (
-          <Board/
-        ) }*/}
-      </div>
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab == "Board" && (
+        <Boardview id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab == "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+
     </div>
   )
 }
