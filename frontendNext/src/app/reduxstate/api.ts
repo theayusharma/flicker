5c13dfcb73372d83a2742df2d0e0cfda8ad4de1a
theayusharma <protocolayush@gmail.com>
2025-08-31 02:51:25 +0530

    some new

--- frontendNext/src/app/reduxstate/api.ts
+++ frontendNext/src/app/reduxstate/api.ts
@@ -3,61 +3,62 @@ import { error } from "console";
 import { PagesUserlandModule } from "next/dist/server/route-modules/pages/module.compiled";
 
 export interface Project {
-    id? :string;
-    name?: string;
-		description?:string;
-		startdate?:string;
-		enddate?:string;
+  id?: string;
+  name?: string;
+  description?: string;
+  startdate?: string;
+  enddate?: string;
 
 }
 
 export interface Task {
 
-	id: number; 
-	createdat?: number; 
-	updatedat?: number; 
-	title: string; 
-	description?: string; 
-	status?: Status; 
-	priority?: Priority; 
-	tags?: string; 
-	startdate?: string; 
-	duedate?: string; 
-	points?: number; 
-	projectid: number; 
-	authorid?: number; 
-	assigneeid?: number; 
-  
-  author?:User;
-  assignee?:User;
-  comments?:Comment[];
-  attachments?:Attachment[]
+  id: number;
+  createdat?: number;
+  updatedat?: number;
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  tags?: string;
+  startdate?: string;
+  duedate?: string;
+  points?: number;
+  projectid: number;
+  authorid?: number;
+  assigneeid?: number;
+
+  author?: User;
+  assignee?: User;
+  comments?: Comment[];
+  attachments?: Attachment[]
 
 }
 export interface Attachment {
-  id : number;
-  fileUrl: string;
-  fileName: string;
-  taskId : number;
-  uploadedId : number;
+  id: number;
+  FileURL: string;
+  FileName: string;
+  taskId: number;
+  uploadedId: number;
 }
 
 export interface User {
   userIdd?: number;
   username: string;
-  email:string;
-  profilePictureUrl?:string;
-  providerId?:string;
-  teamId?:number;
+  email: string;
+  profilePictureUrl?: string;
+  providerId?: string;
+  teamId?: number;
 }
 
 export enum Status {
   todo = "To Do",
-  workinprog = "Work In Progress",
-  completed = "Completed",
-  underreview = "Under Review"
+  in_progress = "Work In Progress",
+  done = "Completed",
+  review = "Under Review"
 
 }
+// Status string`gorm:"type:varchar(50);check:status IN ('todo','in_progress','review','done')" json:"status"`
 export enum Priority {
   urgnet = "Urgnet",
   backlog = "Backlog",
@@ -69,45 +70,58 @@ export enum Priority {
 
 
 export const api = createApi({
-    baseQuery : fetchBaseQuery({baseUrl : process.env.NEXT_PUBLIC_BACK_URL }),
-    reducerPath : 'api',
-    tagTypes : ["Projects", "Tasks"],
-    endpoints: (build) => ({
-    getProjects:build.query<Project[],void>({
-      query : () => "projects" ,
-      providesTags : ["Projects"],
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_BACK_URL }),
+  reducerPath: 'api',
+  tagTypes: ["Projects", "Tasks"],
+  endpoints: (build) => ({
+
+    getProjects: build.query<Project[], void>({
+      query: () => "projects",
+      providesTags: ["Projects"],
     }),
-    createProject : build.mutation<Project, Partial < Project>>({
-      query:(project) => ({
-        url : "projects",
-        method : "POST",
-        body : project
+
+    createProject: build.mutation<Project, Partial<Project>>({
+      query: (project) => ({
+        url: "projects",
+        method: "POST",
+        body: project
       }),
-      invalidatesTags : ["Projects"], 
+      invalidatesTags: ["Projects"],
     }),
-    getTasks:build.query<Task[],{projectId : number}>({
-      query : ( {projectId}) => `tasks?ProjectId = ${projectId}` ,
-      providesTags : (result) => result ? result.map(({id}) => ({type:"Tasks" as const, id })) : [{type : "Tasks" as const}]
+
+    getTasks: build.query<Task[], { projectId: number }>({
+      query: ({ projectId }) => `tasks?projectId=${projectId}`,
+      transformResponse: (response: Task[]) =>
+        response.map(task => ({
+          ...task,
+          attachments: task.attachments ?? []
+        })),
+      providesTags: (result) =>
+        result
+          ? result.map(({ id }) => ({ type: "Tasks" as const, id }))
+          : [{ type: "Tasks" as const }]
     }),
-    createTask  : build.mutation<Task, Partial < Task>>({
-      query:(task ) => ({
-        url : "tasks ",
-        method : "POST",
-        body : task 
+
+    createTask: build.mutation<Task, Partial<Task>>({
+      query: (task) => ({
+        url: "tasks ",
+        method: "POST",
+        body: task
       }),
-      invalidatesTags : ["Tasks"], 
+      invalidatesTags: ["Tasks"],
     }),
-    updateTaskStatus : build.mutation<Task, {taskId : number; status : string}>({
-      query : ({taskId, status}) => ({
-        url : `tasks/${taskId}/status`,
-        method : "PATCH",
-      body : {status}, 
+
+    updateTaskStatus: build.mutation<Task, { taskId: number; status: string }>({
+      query: ({ taskId, status }) => ({
+        url: `tasks/${taskId}/status`,
+        method: "PATCH",
+        body: { status },
       }),
-      invalidatesTags : (result, error, {taskId}) => [{
-        type : "Tasks", id: taskId
+      invalidatesTags: (result, error, { taskId }) => [{
+        type: "Tasks", id: taskId
       }],
     })
   }),
 })
 
-export const {useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation, useUpdateTaskStatusMutation} = api;
+export const { useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation, useUpdateTaskStatusMutation } = api;
