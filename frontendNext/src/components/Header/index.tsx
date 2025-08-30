1540f74c249c5e8938f6f0657cd2dee3dd328295
theayusharma <protocolayush@gmail.com>
2025-08-31 02:51:42 +0530

    more css

--- frontendNext/src/components/Header/index.tsx
+++ frontendNext/src/components/Header/index.tsx
@@ -10,7 +10,12 @@ const Header = ({
 }: Props) => {
 
   return (
-    <div className="mb-5 flex w-full items-center justify-between" ></div>
+    <div className="mb-5 flex w-full items-center justify-between" >
+      <h1 className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold  dark:text-white `}>
+      {name}
+      </h1>
+    {buttonComponent}
+    </div>
 
   )
 }
