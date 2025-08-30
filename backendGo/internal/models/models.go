5890b84b5d78de85228cb20017f2c388e2b74859
theayusharma <protocolayush@gmail.com>
2025-08-31 02:52:05 +0530

    sql model modification

--- backendGo/internal/models/models.go
+++ backendGo/internal/models/models.go
@@ -1,12 +1,12 @@
 package models
 
 import (
-	"time"
 	"gorm.io/gorm"
+	"time"
 )
 
 type Team struct {
-	ID                   uint           `gorm:"column:team_id;primaryKey;autoIncrement"`
+	ID                   uint `gorm:"column:team_id;primaryKey;autoIncrement"`
 	CreatedAt            time.Time
 	UpdatedAt            time.Time
 	DeletedAt            gorm.DeletedAt `gorm:"index"`
@@ -21,7 +21,7 @@ type Team struct {
 func (Team) TableName() string { return "teams" }
 
 type User struct {
-	UserID            uint           `gorm:"column:user_id;primaryKey;autoIncrement"`
+	UserID            uint `gorm:"column:user_id;primaryKey;autoIncrement"`
 	CreatedAt         time.Time
 	UpdatedAt         time.Time
 	DeletedAt         gorm.DeletedAt `gorm:"index"`
@@ -33,17 +33,17 @@ type User struct {
 	ProfilePictureURL *string        `gorm:"type:text"`
 	TeamID            *uint          `gorm:"index"`
 
-	Team            *Team           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:TeamID"`
-	AuthoredTasks   []Task          `gorm:"foreignKey:AuthorID;references:UserID"`
-	AssignedTasks   []Task          `gorm:"foreignKey:AssigneeID;references:UserID"`
+	Team            *Team            `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:TeamID"`
+	AuthoredTasks   []Task           `gorm:"foreignKey:AuthorID;references:UserID"`
+	AssignedTasks   []Task           `gorm:"foreignKey:AssigneeID;references:UserID"`
 	TaskAssignments []TaskAssignment `gorm:"foreignKey:UserID"`
-	Attachments     []Attachment    `gorm:"foreignKey:UploadedByID"`
+	Attachments     []Attachment     `gorm:"foreignKey:UploadedByID"`
 }
 
 func (User) TableName() string { return "users" }
 
 type Project struct {
-	ID          uint           `gorm:"column:project_id;primaryKey;autoIncrement"`
+	ID          uint `gorm:"column:project_id;primaryKey;autoIncrement"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
@@ -59,7 +59,7 @@ type Project struct {
 func (Project) TableName() string { return "projects" }
 
 type ProjectTeam struct {
-	ID        uint           `gorm:"column:project_team_id;primaryKey;autoIncrement"`
+	ID        uint `gorm:"column:project_team_id;primaryKey;autoIncrement"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
@@ -73,33 +73,34 @@ type ProjectTeam struct {
 func (ProjectTeam) TableName() string { return "project_teams" }
 
 type Task struct {
-	ID          uint           `gorm:"column:task_id;primaryKey;autoIncrement"json:"id"`
+	ID          uint `gorm:"column:task_id;primaryKey;autoIncrement"json:"id"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   gorm.DeletedAt `gorm:"index"`
 	Title       string         `gorm:"not null"`
 	Description *string        `gorm:"type:text"`
-	Status      *string        `gorm:"type:varchar(50)"`
-	Priority    *string        `gorm:"type:varchar(50)"`
-	Tags        *string        `gorm:"type:text"`
-	StartDate   *time.Time
-	DueDate     *time.Time
-	Points      *uint
-	ProjectID   uint           `gorm:"not null;index"json:"project_id"`
-	AuthorID    uint           `gorm:"not null;index"`
-	AssigneeID  *uint          `gorm:"index"`
+	// Status      *string        `gorm:"type:varchar(50)"`
+	Status     *string `gorm:"type:varchar(50);check:status IN ('todo','in_progress','review','done')" json:"status"`
+	Priority   *string `gorm:"type:varchar(50)"`
+	Tags       *string `gorm:"type:text"`
+	StartDate  *time.Time
+	DueDate    *time.Time
+	Points     *uint
+	ProjectID  uint  `gorm:"not null;index"json:"project_id"`
+	AuthorID   uint  `gorm:"not null;index"`
+	AssigneeID *uint `gorm:"index"`
 
-	Project         Project         `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ProjectID"`
-	Author          User            `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:AuthorID;references:UserID"`
-	Assignee        *User           `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:AssigneeID;references:UserID"`
+	Project         Project          `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:ProjectID"`
+	Author          User             `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:AuthorID;references:UserID"`
+	Assignee        *User            `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;foreignKey:AssigneeID;references:UserID"`
 	TaskAssignments []TaskAssignment `gorm:"foreignKey:TaskID"`
-	Attachments     []Attachment    `gorm:"foreignKey:TaskID"`
+	Attachments     []Attachment     `json:"attachments" gorm:"foreignKey:TaskID"`
 }
 
 func (Task) TableName() string { return "tasks" }
 
 type TaskAssignment struct {
-	ID        uint           `gorm:"column:task_assi_id;primaryKey;autoIncrement"`
+	ID        uint `gorm:"column:task_assi_id;primaryKey;autoIncrement"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
@@ -113,7 +114,7 @@ type TaskAssignment struct {
 func (TaskAssignment) TableName() string { return "task_assignments" }
 
 type Attachment struct {
-	ID           uint           `gorm:"column:attachment_id;primaryKey;autoIncrement"`
+	ID           uint `gorm:"column:attachment_id;primaryKey;autoIncrement"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 	DeletedAt    gorm.DeletedAt `gorm:"index"`
@@ -128,15 +129,14 @@ type Attachment struct {
 
 func (Attachment) TableName() string { return "attachments" }
 
-
 type Comment struct {
-	ID     uint           `gorm:"column:id;primaryKey;autoIncrement"`
-	Text   string         `gorm:"type:text;not null"`
-	TaskID uint           `gorm:"not null;index"`
-	UserID uint           `gorm:"not null;index"`
+	ID     uint   `gorm:"column:id;primaryKey;autoIncrement"`
+	Text   string `gorm:"type:text;not null"`
+	TaskID uint   `gorm:"not null;index"`
+	UserID uint   `gorm:"not null;index"`
 
 	Task Task `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:TaskID;references:ID"`
 	User User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserID;references:UserID"`
 }
 
-func (Comment) TableName() string {return "comment"}
+func (Comment) TableName() string { return "comment" }
