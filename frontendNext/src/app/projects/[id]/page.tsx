82c9a519de41a142194e06f765f1d945b06627e9
theayusharma <protocolayush@gmail.com>
2025-09-01 06:52:22 +0530

    new routing of projects

--- frontendNext/src/app/projects/[id]/page.tsx
+++ frontendNext/src/app/projects/[id]/page.tsx
@@ -1,1 +1,39 @@
+"use client"
+import React, { useState } from "react"
+import ProjectHeader from "@/app/projects/ProjectHeader"
+import Boardview from "../Boardview";
+import List from "../ListView";
+import TimeLineView from "../TimelineView";
+import TableView from "../TableView";
+type Props = {
+  params: { id: string };
+};
 
+const Project = ({ params }: Props) => {
+  const { id } = React.use(params);
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
+
+
+  return (
+    // modal tasks
+    <div className="dark:bg-zinc-800">
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab == "Board" && (
+        <Boardview id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab == "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab == "Timeline" && (
+        <TimeLineView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab == "Table" && (
+        <TableView id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+
+    </div>
+  )
+}
+
+export default Project
